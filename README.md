c40def1a70733b58394b8656c34af532e58c7a91
JoaoMafort <167534691+JoaoMafort@users.noreply.github.com>
2025-11-18 10:12:37 -0300

    adicionado modo escuro

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # MolLab
-<>
-
-O site visa ensinar de forma divertida atendendo a estudantes do ensino médio, bem como professores que buscaram uma ferramenta de apoio para o ensino e aprendizado de química, abrangendo desde conceitos básicos até conteúdos avançados. Nosso objetivo é facilitar o aprendizado de química por meio de um ambiente organizado e interativo, oferecendo teoria, prática virtual (experimentos simulados) e exercícios. O sistema busca ser uma ponte entre o conteúdo escolar e a aplicabilidade real da química, com foco na didática e na compreensão gradual.
--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function menu() {
 
-    var x = document.getElementById("panel");  
+    var x = document.getElementById("panel");
     if (x.style.display === "none") {
         x.style.display = "flex";
     } else {
         x.style.display = "none";
     }
 }
+
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+  
+    // Salva a preferência do usuário
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  }
+  
+  // Aplica o tema salvo ao carregar a página
+  window.onload = function() {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+    }
+  };
